4a728b70aa714ce2d46d4aa953fcd9729ffb5048
mayro.belizario <mayro.belizario@trinusco.com.br>
2024-09-04 19:50:38 -0300

    feat: :sparkles: add: feature flas para eventos e testes de /eventos

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,12 +1,20 @@
 import Evento from '../models/evento.js';
 
 class EventosController {
+
+  static liberaAcessoEventos = () => process.env.EVENTO_FLAG === 'true';
+
   static async listarEvento(req, res) {
-    try {
-      const eventos = await Evento.pegarEventos();
-      return res.status(200).json(eventos);
-    } catch (err) {
-      return res.status(404).json({ message: err.message });
+
+    if (EventosController.liberaAcessoEventos()) {
+      try {
+        const eventos = await Evento.pegarEventos();
+        return res.status(200).json(eventos);
+      } catch (err) {
+        return res.status(404).json({ message: err.message });
+      }
+    } else {
+      return res.status(404).json({ message: 'Função não encontrada' });
     }
   }
 }
